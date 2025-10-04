d45989bb78f9cd5c432e10aafb95384df4bf815f
Tman1314 <tman1314@hotmail.com>
2025-10-04 12:25:47 -0700

    Added warning message to Login page.

--- scripts/login.js
+++ scripts/login.js
@@ -2,12 +2,19 @@ const magicLinkBtn = document.getElementById('magicLinkBtn');
 const email = document.getElementById('email');
 const thirdPartyBtns = document.querySelectorAll('.thirdPartyBtn');
 const loginForm = document.getElementById('loginForm');
+const dialogCloseBtn = document.getElementById("closeDialogBtn");
+const warningDialog = document.getElementById("warningDialog");
+
 
 thirdPartyBtns.forEach(element => {
     element.addEventListener('click', () => {
         window.location.href = '/home';
     });
 });
+
+dialogCloseBtn.addEventListener("click", () => {
+    warningDialog.close();
+})
 
 loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
